8f1837649c1dc567aa4518ee2af15a39902d36b8
geetanjali aggarwal <gitanjali_bawa@yahoo.in>
2019-03-04 12:32:48 +1100

    First commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,13 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void firsttestrest()
+	{
+		System.out.println("FirstRestAPItest");
+	}
+
+
+}
